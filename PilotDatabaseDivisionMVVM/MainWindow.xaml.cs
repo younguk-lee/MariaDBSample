f5bf24d09e1644899bb0d958be1480a968a726cc
younguk-lee <younguk.lee@pnesolution.com>
2021-02-15 10:50:38 +0900

    first commit

---
+++ PilotDatabaseDivisionMVVM/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace PilotDatabaseDivisionMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 버튼 클릭시 데이터를 불러오는 작업
+        private void btnloaddata_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "SERVER=localhost;DATABASE=Code;UID=root;PASSWORD=rnd12#$;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM divisioncode", connection);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                // 윈도우 폼의 LoadDataBinding에 데이터 넣기
+                adp.Fill(ds, "LoadDataBinding");
+                dataGridCustomers.DataContext = ds;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
